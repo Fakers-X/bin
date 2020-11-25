b2674ce5744047de63c4ef1148c855394dbb4348
Randi-ID <66753543+Rendi-ID@users.noreply.github.com>
2020-11-25 11:34:45 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+clear
+echo -e " \033[1;97m{\033[1;96m•\033[1;97m} \033[1;92mInstalling Packages.."
+sleep 1
+apt update
+apt upgrade
+apt install python -y
+apt install bash
+apt install figlet
+gem install lolcat
+pip install requests
+pip install youtube-dl
